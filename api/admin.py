9ed2eee1a483bd615ecdce79c8c2295a5e45d218
Charitra Agarwal <charitraagarwal1022@gmail.com>
2022-07-21 13:33:49 +0530

    added models

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import CodeChefProfile, LeetCodeProfile, HackerRankProfile, GFGProfile
 
-# Register your models here.
+admin.site.register(GFGProfile)
+admin.site.register(CodeChefProfile)
+admin.site.register(LeetCodeProfile)
+admin.site.register(HackerRankProfile)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+class GFGProfile(models.Model):
+    username = models.CharField(max_length=20, primary_key=True)
+    platform = models.CharField(max_length=20)
+    problems = models.PositiveIntegerField(default=0)
+
+    def __str__(self) -> str:
+        msg = f"{self.platform}:{self.username}"
+        return msg
+
+
+class CodeChefProfile(models.Model):
+    username = models.CharField(max_length=20, primary_key=True)
+    platform = models.CharField(max_length=20)
+    stars = models.PositiveIntegerField(default=1)
+    rating = models.PositiveIntegerField(default=0)
+
+    def __str__(self) -> str:
+        msg = f"{self.platform}:{self.username}"
+        return msg
+
+
+class LeetCodeProfile(models.Model):
+    username = models.CharField(max_length=20, primary_key=True)
+    platform = models.CharField(max_length=20)
+    problems = models.PositiveIntegerField(default=0)
+
+    def __str__(self) -> str:
+        msg = f"{self.platform}:{self.username}"
+        return msg
+
+
+class HackerRankProfile(models.Model):
+    username = models.CharField(max_length=20, primary_key=True)
+    platform = models.CharField(max_length=20)
+    stars = models.PositiveIntegerField(default=1)
+
+    def __str__(self) -> str:
+        msg = f"{self.platform}:{self.username}"
+        return msg
