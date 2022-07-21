c1c62a4484aa48a088732367798733d3b5e5eccc
Charitra Agarwal <charitraagarwal1022@gmail.com>
2022-07-21 13:37:00 +0530

    added parameterized url format

--- coding_profile/urls.py
+++ coding_profile/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+
+# from rest_framework.urlpatterns import format_suffix_patterns
+from api import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    re_path(r'^codingprofile/$', views.GetDetails.as_view()),
+    
 ]
