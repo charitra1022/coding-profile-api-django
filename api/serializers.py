87eca4b0fca15756487ec3b7cf72ee51aad5189f
Charitra Agarwal <charitraagarwal1022@gmail.com>
2022-07-21 13:34:19 +0530

    added serializers

---
+++ api/serializers.py
@@ -0,0 +1,30 @@
+from dataclasses import field, fields
+from rest_framework import serializers
+from .models import CodeChefProfile, LeetCodeProfile, HackerRankProfile, GFGProfile
+
+
+class GFGProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GFGProfile
+        fields = ('username', 'platform', 'problems')
+
+
+class CodeChefProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CodeChefProfile
+        fields = ('username', 'platform', 'stars', 'rating')
+
+
+class LeetCodeProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LeetCodeProfile
+        fields = ('username', 'platform', 'problems')
+
+
+class HackerRankProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = HackerRankProfile
+        fields = ('username', 'platform', 'stars')
+
+
+
