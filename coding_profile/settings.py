1b59791ddb41918141db18f34122fb7dfcc72e22
Charitra Agarwal <charitraagarwal1022@gmail.com>
2022-07-25 21:19:42 +0530

    [bug]: (static files not working in prod) added static files location

--- coding_profile/settings.py
+++ coding_profile/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'api/static'   ## added static url
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
